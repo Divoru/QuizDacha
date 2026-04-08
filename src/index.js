e96b40493e28d13992e612c6732ec2caba83db40
Divoru <155429314+Divoru@users.noreply.github.com>
2026-04-08 10:04:19 +0300

    v0.02

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import { createRoot } from "react-dom/client";
 import DigitalDachaApp from "./App";
 import "./styles.css";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
+const root = createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
